ab55c1c7047672b0e9363c81477d6e64644fd8df
Kory Sasaoka <kory.sasaoka@gmail.com>
2017-04-11 00:28:49 -0400

    ajax call to spotify API, finished header structure and styling

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,8 +1,15 @@
-var Trackster = {};
+$(document).ready(function() {
+  $('#search-icon').click(function() {
+    var text = $('#search-bar').val();
 
+    Trackster.searchTracksByTitle(text);
+  });
+});
+
+var Trackster = {};
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks. 
+  Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
 
@@ -13,5 +20,9 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
+  var url = 'https://api.spotify.com/v1/search?type=track&q=' + title;
 
+  $.get(url, function(data, status) {
+      console.log(data);
+  });
 };
